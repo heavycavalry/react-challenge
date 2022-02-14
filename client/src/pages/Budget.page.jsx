ad2780840e883ceeb9d0fcb996a795d2cba84885
heavycavalry <klaudia.diana.paluch@gmail.com>
2022-02-14 23:33:24 +0100

    task-2: date cells

--- client/src/pages/Budget.page.jsx
+++ client/src/pages/Budget.page.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import {ActionHeader, Card, Page, Table} from 'ui';
+import {Grid} from '@mui/material';
 
-import { ActionHeader, Card, Page } from 'ui';
-import { Grid } from '@mui/material';
+let row =  {
+    "createdAt": 1641037200000,
+    "categoryId": "8",
+    "amountInCents": 10000,
+    "id": "7",
+    "currentSpending": 11799,
+    "currentSpendingPercent": 117,
+    "category": {
+        "id": "8",
+        "name": "Różne",
+        "color": "#93ab8e",
+        "budgetId": "7",
+        "ledgerIds": [
+            "15",
+            "16"
+        ],
+        "createdAt": 1643713800000
+    }
+}
+const CreateDateCell = {
+    id: "createdAt",
+    renderCell(row) {
+        const ts = row["createdAt"];
+        const date = new Date(ts);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const day = date.getDay();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}, ${hours}:${minutes}:${seconds}`;
+    }
+}
+
+
 
 export const BudgetPage = () => {
   return (
@@ -16,7 +51,9 @@ export const BudgetPage = () => {
         }
       >
         <Grid container>
-          <Grid item xs={12}></Grid>
+          <Grid item xs={12}>
+              <Table headCells={[CreateDateCell]} rows={[row]}/>
+          </Grid>
         </Grid>
       </Card>
     </Page>
