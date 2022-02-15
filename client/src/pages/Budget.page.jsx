e88f754783392067eb978d0627830fb28506675d
heavycavalry <klaudia.diana.paluch@gmail.com>
2022-02-15 12:21:48 +0100

    task-2: name column

--- client/src/pages/Budget.page.jsx
+++ client/src/pages/Budget.page.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {ActionHeader, Card, Page, Table} from 'ui';
+import {ActionHeader, Button, Card, Page, Table} from 'ui';
 import {Grid} from '@mui/material';
-
-let row =  {
+import styled from 'styled-components'
+import {IconAdd} from "../ui/atoms/button.styled";
+const row =  {
     "createdAt": 1641037200000,
     "categoryId": "8",
     "amountInCents": 10000,
@@ -20,23 +21,52 @@ let row =  {
         ],
         "createdAt": 1643713800000
     }
+
 }
-const CreateDateCell = {
-    id: "createdAt",
+const DateCells = {
+    label: "Data utworzenia",
     renderCell(row) {
         const ts = row["createdAt"];
         const date = new Date(ts);
         const hours = date.getHours();
         const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
-        const day = date.getDay();
-        const month = date.getMonth();
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+        const day = String(date.getDay()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
         const year = date.getFullYear();
         return `${day}.${month}.${year}, ${hours}:${minutes}:${seconds}`;
     }
 }
+const NameCells = {
+    label: "Nazwa",
+    renderCell(row) {
+        const category = row["category"];
+        return <IconBox color={category.color}>{category.name}</IconBox>;
+    }
+}
+
+const ExpensesCells = {
+    label: "Planowane wydatki",
+    renderCell(row) {
+        const category = row["category"];
+        return <IconBox color={category.color}>{category.name}</IconBox>;
+    }
+}
 
 
+
+const getUniqueId = () => row.id;
+const IconBox = styled.div`
+&:before {
+  content: "";
+  display: block;
+  float: left;
+  margin-right: 10px;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props => props.color};
+}`;
 
 export const BudgetPage = () => {
   return (
@@ -45,17 +75,18 @@ export const BudgetPage = () => {
         title={
           <ActionHeader
             variant={'h1'}
-            title="Budżet"
+            title="Twój budżet"
             renderActions={() => null}
           />
         }
       >
         <Grid container>
           <Grid item xs={12}>
-              <Table headCells={[CreateDateCell]} rows={[row]}/>
+              <Table headCells={[ NameCells, ExpensesCells, DateCells]} rows={[row]} getUniqueId={getUniqueId} deleteRecords={[]}/>
           </Grid>
         </Grid>
       </Card>
+        <Button variant={"contained"} color={"primary"} startIcon={<IconAdd/>} label={"Zdefiniuj budżet"}/>
     </Page>
   );
 };
