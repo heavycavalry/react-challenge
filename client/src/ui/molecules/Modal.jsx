5655e0a5194f2010550b43cf03ec3b03314f74e8
Klaudia Paluch <klaudia.diana.paluch@gmail.com>
2022-02-23 16:53:26 +0100

    modal.jsx

---
+++ client/src/ui/molecules/Modal.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Modal as MuiModal } from '@mui/material';
+import { Card } from '@mui/material';
+import { Button } from 'ui';
+import { CardHeader, CardContent } from '@mui/material';
+import { CardActions as MuiCardActions } from '@mui/material';
+import styled from 'styled-components';
+
+export const Modal = ({ title, children, description }) => {
+  const [open, setOpen] = useState(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <StyledModal
+      outline={0}
+      BackdropComponent={Backdrop}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="parent-modal-title"
+      aria-describedby="parent-modal-description"
+    >
+      <Card sx={{ width: 450 }} variant="outlined">
+        <CardHeader title={title} />
+        <h4>{description}</h4>
+        <CardContent sx={{ minHeight: 20 }} children={children} />
+        <MuiCardActions sx={{ float: 'right' }}>
+          <Button variant="outlined" color="primary" onClick={handleClose}>
+            Anuluj
+          </Button>
+          <Button variant="contained" color="primary">
+            Zapisz
+          </Button>
+        </MuiCardActions>
+      </Card>
+    </StyledModal>
+  );
+};
+
+const StyledModal = styled(MuiModal)`
+  position: fixed;
+  z-index: 1300;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Backdrop = styled('div')`
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-tap-highlight-color: transparent;
+`;
