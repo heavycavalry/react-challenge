7f138fec27773031c9115721074bdc48df96fed5
klaudia.paluch <klaudia.diana.paluch@gmail.com>
2022-03-12 14:50:33 +0100

    budget form, rollback 'extract Form component' bc of return type

--- client/src/ui/organisms/AddNewBudgetRecord.modal.jsx
+++ client/src/ui/organisms/AddNewBudgetRecord.modal.jsx
@@ -1,8 +1,33 @@
 import {Modal} from 'ui/molecules/Modal';
+import {Controller, useForm} from "react-hook-form";
+import {FormControl, TextField} from "@mui/material";
+import * as React from "react";
+import {SelectField} from "./AddNewLedgerRecord.modal";
 
 export const AddNewBudgetRecord = ({isOpen, setIsOpen}) => {
+    const {handleSubmit, control} = useForm();
+    const onSubmit = (data) => console.log(data);
     if (isOpen) {
-        return <Modal title="Zdefiniuj budżet" isOpen={isOpen} setIsOpen={setIsOpen}/>;
+        return (
+            <Modal title="Zdefiniuj budżet" isOpen={isOpen} setIsOpen={setIsOpen} onSubmit={handleSubmit(onSubmit)}>
+                <FormControl fullWidth>
+                    <Controller
+                        name={'amount'}
+                        control={control}
+                        render={({field: {onChange, value}}) => (
+                            <TextField onChange={onChange} value={value} label={'Kwota'}/>
+                        )}
+                    />
+                    <Controller
+                        name={'category'}
+                        control={control}
+                        render={({field: {onChange, value}}) => (
+                            <SelectField onChange={onChange} value={value}/>
+                        )}
+                    />
+                </FormControl>
+            </Modal>
+        );
     }
     return <></>;
 };
