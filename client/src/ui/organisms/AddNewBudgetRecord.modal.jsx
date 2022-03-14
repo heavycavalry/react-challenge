1109cf9e2d07ac0cf21c8c0a1481a832b8a7f3fd
klaudia.paluch <klaudia.diana.paluch@gmail.com>
2022-03-14 22:03:12 +0100

    ref components to pass tests

--- client/src/ui/organisms/AddNewBudgetRecord.modal.jsx
+++ client/src/ui/organisms/AddNewBudgetRecord.modal.jsx
@@ -1,24 +1,28 @@
 import {Modal} from 'ui/molecules/Modal';
 import {Controller, useForm} from 'react-hook-form';
-import {FormControl, TextField} from '@mui/material';
+import {TextField} from '@mui/material';
 import * as React from 'react';
 import {SelectField} from './AddNewLedgerRecord.modal';
 import {yupResolver} from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
+import {CategoryService} from "../../api";
 
 export const AddNewBudgetRecord = ({isOpen, setIsOpen}) => {
     const validationSchema = Yup.object().shape({
         amount: Yup.number()
             .required('Kwota nie może być pusta')
-            .typeError('Zły format kwoty')
             .min(0, 'Kwota musi być większa niż 0')
             .max(1000000, 'Kwota nie może być większa niż 1000000'),
         category: Yup.string()
-            .required('Wybierz kategorie')
-
+            .required('Wybierz kategorie'),
     });
-    const {handleSubmit, control, reset, formState: {errors}} = useForm({
-        resolver: yupResolver(validationSchema)
+    const {
+        handleSubmit,
+        control,
+        reset,
+        formState: {errors},
+    } = useForm({
+        resolver: yupResolver(validationSchema),
     });
     const onSubmit = (data) => console.log(data);
     if (isOpen) {
@@ -30,25 +34,25 @@ export const AddNewBudgetRecord = ({isOpen, setIsOpen}) => {
                 onSubmit={handleSubmit(onSubmit)}
                 reset={() => reset()}
             >
-                <FormControl fullWidth>
-                    <Controller
-                        name={'amount'}
-                        control={control}
-                        render={({field: {onChange, value}}) => (
-                            <TextField
-                                onChange={onChange} value={value} label={'Kwota'}/>
-                        )}
-                    />
-                    <>{errors.amount?.message}</>
-                    <Controller
-                        name={'category'}
-                        control={control}
-                        render={({field: {onChange, value}}) => (
-                            <SelectField onChange={onChange} value={value}/>
-                        )}
-                    />
-                    <>{errors.category?.message}</>
-                </FormControl>
+                <Controller
+                    name={'amount'}
+                    control={control}
+                    render={({field: {onChange, value}}) => (
+                        <TextField fullWidth
+                                   type={"number"} error={errors.amount} helperText={errors.amount?.message}
+                                   onChange={onChange}
+                                   value={value} label={'Kwota'}/>
+                    )}
+                />
+                <Controller
+                    name={'category'}
+                    control={control}
+                    render={({field: {onChange, value}}) => (
+                        <SelectField fullWidth
+                                     onChange={onChange} value={value} errors={errors}
+                                     method={CategoryService.findAll(true)}/>
+                    )}
+                />
             </Modal>
         );
     }
--- client/src/ui/organisms/AddNewLedgerRecord.modal.jsx
+++ client/src/ui/organisms/AddNewLedgerRecord.modal.jsx
@@ -1,123 +1,123 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import {Modal} from 'ui/molecules/Modal';
-import {FormControl, Select, TextField} from '@mui/material';
+import {FormHelperText, Select, TextField} from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import {CategoryService} from '../../api';
-import {useEffect, useState} from 'react';
 import {CategoryCell} from '../molecules/CategoryCell';
 import {Controller, useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
+import {KeyboardArrowDown} from "@mui/icons-material";
 
 export const AddNewLedgerRecord = ({type, isOpen, setIsOpen}) => {
     let shape = {
         name: Yup.string()
-            .required('Nazwa nie może być pusta'),
-        amount: Yup.number()
+            .trim()
+            .required('Nazwa nie może być pusta'), amount: Yup.number()
             .required('Kwota nie może być pusta')
             .typeError('Zły format kwoty')
             .min(0, 'Kwota musi być większa niż 0')
-            .max(1000000, 'Kwota nie może być większa niż 1000000')
-    }
+            .max(1000000, 'Kwota nie może być większa niż 1000000'),
+    };
     if (type === 'EXPENSE') {
         shape.category = Yup.string().required('Wybierz kategorię');
     }
     const validationSchema = Yup.object().shape(shape);
+
     const {handleSubmit, control, reset, formState: {errors}} = useForm({
         resolver: yupResolver(validationSchema)
     });
     const onSubmit = (data) => console.log(data);
 
     if (type === 'EXPENSE') {
-        return (
-            <Modal
-                title="Dodaj wydatek"
-                isOpen={isOpen}
-                setIsOpen={setIsOpen}
-                onSubmit={handleSubmit(onSubmit)}
-                reset={() => reset()}
-            >
-                <FormControl fullWidth>
-                    <Controller
-                        name={'name'}
-                        control={control}
-                        render={({field: {onChange, value}}) => (
-                            <TextField onChange={onChange} value={value} label={'Nazwa'}/>
-                        )}
-                    />
-                    <>{errors.name?.message}</>
-                    <Controller
-                        name={'amount'}
-                        control={control}
-                        render={({field: {onChange, value}}) => (
-                            <TextField onChange={onChange} value={value} label={'Kwota'}/>
-                        )}
-                    />
-                    <>{errors.amount?.message}</>
-                    <Controller
-                        control={control}
-                        name={'category'}
-                        render={({field: {onChange, value}}) => (
-                            <SelectField onChange={onChange} value={value}/>
-                        )}
-                    />
-                    {errors.category?.message}
-                </FormControl>
-
-            </Modal>)
+        return (<Modal
+            title="Dodaj wydatek"
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            onSubmit={handleSubmit(onSubmit)}
+            reset={() => reset()}
+        >
+            <Controller
+                name={'name'}
+                control={control}
+                render={({field: {onChange, value}}) => (
+                    <TextField fullWidth type={"text"} margin={"dense"} error={errors.name}
+                               helperText={errors.name?.message}
+                               onChange={onChange} value={value} label={'Nazwa'}/>)}
+            />
+            <Controller
+                name={'amount'}
+                control={control}
+                render={({field: {onChange, value}}) => (
+                    <TextField type={"number"} fullWidth margin={"dense"} error={errors.amount}
+                               helperText={errors.amount?.message}
+                               onChange={onChange}
+                               value={value} label={'Kwota'}/>)}
+            />
+            <Controller
+                control={control}
+                name={'category'}
+                render={({field: {onChange, value}}) => (<SelectField errors={errors} onChange={onChange} value={value}
+                                                                      method={CategoryService.findAll()}
+                />)}
+            />
+        </Modal>);
     }
     if (type === 'INCOME') {
-        return (
-            <Modal
-                title="Dodaj wpływ"
-                isOpen={isOpen}
-                setIsOpen={setIsOpen}
-                onSubmit={handleSubmit(onSubmit)}
-                reset={() => reset()}
-            >
-                <FormControl fullWidth>
-                    <Controller
-                        name={'name'}
-                        control={control}
-                        render={({field: {onChange, value}}) => (
-                            <TextField onChange={onChange} value={value} label={'Nazwa'}/>
-                        )}
-                    />
-                    <>{errors.name?.message}</>
-                    <Controller
-                        name={'amount'}
-                        control={control}
-                        render={({field: {onChange, value}}) => (
-                            <TextField onChange={onChange} value={value} label={'Kwota'}/>
-                        )}
-                    />
-                    <>{errors.amount?.message}</>
+        return (<Modal
+            title="Dodaj wpływ"
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            onSubmit={handleSubmit(onSubmit)}
+            reset={() => reset()}
+        >
+            <Controller
 
-                </FormControl>
-            </Modal>
-        );
+                name={'name'}
+                control={control}
+                render={({field: {onChange, value}}) => (
+                    <TextField fullWidth error={errors.name} type={"text"} helperText={errors.name?.message}
+                               margin={"dense"}
+                               onChange={onChange} value={value} label={'Nazwa'}/>)}
+            />
+
+            <Controller
+                name={'amount'}
+                control={control}
+                render={({field: {onChange, value}}) => (
+                    <TextField fullWidth type={"number"} error={errors.amount} helperText={errors.amount?.message}
+                               margin={"dense"}
+                               onChange={onChange} value={value} label={'Kwota'}/>)}
+            />
+        </Modal>);
     }
     return <></>;
-}
+};
 
-export const SelectField = ({onChange, value}) => {
+export const SelectField = ({onChange, value, errors, method}) => {
     const [categories, setCategories] = useState([]);
     useEffect(() => {
-        const promise = CategoryService.findAll();
-        promise.then((data) => setCategories(data));
+        method.then((data) => setCategories(data));
     }, []);
-    return (
+    return (<>
         <Select
-            label="Wybierz kategorię"
             fullWidth
             onChange={onChange}
             value={value}
+            error={errors.category}
+            defaultValue={"kategoria"}
+            sx={{margin: "10px 0"}}
+            IconComponent={() => <KeyboardArrowDown sx={{color: "#334ACC", margin: "10px", cursor: "pointer"}}/>}
         >
-            {categories.map((category) => (
-                <MenuItem key={category.name} value={category.name}>
-                    <CategoryCell name={category.name} color={category.color}/>
-                </MenuItem>
-            ))}
+            <option value="kategoria" selected disabled
+                    hidden>Kategoria
+            </option>
+            {categories.map((category) => (<MenuItem sx={{margin: "5px 0"}} key={category.name} value={category.name}>
+                <CategoryCell name={category.name} color={category.color}/>
+            </MenuItem>))}
+
         </Select>
-    );
+        <FormHelperText error={errors.category}>{errors.category?.message}</FormHelperText>
+    </>)
 };
